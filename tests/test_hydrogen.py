8bceefd5f4dea0a2d7fed0f96fb35acb32429b6c
robfairh <ref3@illinois.edu>
2021-01-07 14:05:21 -0300

    unit test for internat case

--- tests/test_hydrogen.py
+++ tests/test_hydrogen.py
@@ -1,2 +1,17 @@
-import pytest
+import numpy as np
+import sys
 
+sys.path.append("../hydro-requirement")
+import hydrogen as h2
+
+
+def test_efficiency_internal():
+    """ Tests if the function function works correctly for an internal point
+        (temperature = 50 C) """
+
+    exp = 0.04842
+    obs = h2.efficiency(50)
+    if np.abs(exp - obs) > 1e-3:
+    raise ValueError("Efficincy doesn't work")
+    assert(True)
+    # assert np.abs(exp - obs) < 1e-3
